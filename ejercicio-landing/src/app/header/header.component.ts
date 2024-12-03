3a9c6556f6a32079194f2f1c57a729b959191780
Andrea Roldán <andrea-roldan@outlook.es>
2024-12-03 13:49:45 +0100

    Empezando navbar

--- ejercicio-landing/src/app/header/header.component.ts
+++ ejercicio-landing/src/app/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  esModoOscuro: boolean = false;
 
+  cambiarModo() {
+    this.esModoOscuro = !this.esModoOscuro;
+  }
 }
