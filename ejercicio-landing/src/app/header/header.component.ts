675e12ae2df6c79cd762fcad53200f389aea8d44
Andrea Roldán <andrea-roldan@outlook.es>
2024-12-09 12:39:32 +0100

    Añadidos el banner y el formulario de busqueda de pelicula no funcional

--- ejercicio-landing/src/app/header/header.component.ts
+++ ejercicio-landing/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -14,6 +15,8 @@ export class HeaderComponent {
   isFuentePequenia: boolean = false;
   fuentePequenia:string = "var(--fuente-pequenia)";
 
+  tituloPeli: string = "";
+  generoPeli: string = "";
 
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
