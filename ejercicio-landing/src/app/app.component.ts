edf9e3b4ad1ffad578e583287fb9d8e912145ff4
xevidomi <xevidominguez@gmail.com>
2024-12-09 13:07:08 +0100

    nuevo cambio

--- ejercicio-landing/src/app/app.component.ts
+++ ejercicio-landing/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';  // Añadir FormsModule
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Component, Input, NgModule, BrowserModule],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
